6f487d98450696fdac7af7f10335f97400003247
Leonardo Hipolito <dev@jointech.io>
2022-04-06 10:36:19 -0300

    primeiro commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login', () => {
+    it('Deve fazre login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain','Minha conta')
+    });
+    it('deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebacd@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido')
+    });
+    it('deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('asdfasdf')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain','A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta')
+    });
+});
